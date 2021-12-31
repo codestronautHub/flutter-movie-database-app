9671b01a96849b31426c1a554b61009d929c24bb
codestronaut <hello.adityarohman@gmail.com>
2021-12-31 11:23:22 +0800

    add: domain on air tv (include test)

---
+++ lib/domain/repositories/tv_repository.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/domain/entities/tv.dart';
+
+abstract class TvRepository {
+  Future<Either<Failure, List<Tv>>> getOnTheAirTvs();
+}
--- test/helpers/test_helper.dart
+++ test/helpers/test_helper.dart
@@ -2,11 +2,13 @@ import 'package:ditonton/data/datasources/db/database_helper.dart';
 import 'package:ditonton/data/datasources/movie_local_data_source.dart';
 import 'package:ditonton/data/datasources/movie_remote_data_source.dart';
 import 'package:ditonton/domain/repositories/movie_repository.dart';
+import 'package:ditonton/domain/repositories/tv_repository.dart';
 import 'package:mockito/annotations.dart';
 import 'package:http/http.dart' as http;
 
 @GenerateMocks([
   MovieRepository,
+  TvRepository,
   MovieRemoteDataSource,
   MovieLocalDataSource,
   DatabaseHelper,
