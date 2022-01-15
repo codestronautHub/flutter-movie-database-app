a1c765dcbc3c87aa541805a475e72e5cc85dee90
codestronaut <hello.adityarohman@gmail.com>
2022-01-15 22:51:37 +0800

    add: watchlist tv notifier (with test)

--- test/presentation/pages/tv_detail_page_test.dart
+++ test/presentation/pages/tv_detail_page_test.dart
@@ -1,25 +1,62 @@
 import 'package:ditonton/presentation/provider/tv_detail_notifier.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
-// import 'package:provider/provider.dart';
+import 'package:mockito/mockito.dart';
+import 'package:provider/provider.dart';
 
-// import 'tv_detail_page_test.mocks.dart';
+import 'tv_detail_page_test.mocks.dart';
 
 @GenerateMocks([TvDetailNotifier])
 void main() {
-  // late MockTvDetailNotifier mockNotifier;
+  late MockTvDetailNotifier mockNotifier;
 
-  // setUp(() {
-  //   mockNotifier = MockTvDetailNotifier();
-  // });
+  setUp(() {
+    mockNotifier = MockTvDetailNotifier();
+  });
 
-  // Widget _makeTestableWidget(Widget body) {
-  //   return ChangeNotifierProvider<TvDetailNotifier>.value(
-  //     value: mockNotifier,
-  //     child: MaterialApp(
-  //       home: body,
-  //     ),
-  //   );
-  // }
+  Widget _makeTestableWidget(Widget body) {
+    return ChangeNotifierProvider<TvDetailNotifier>.value(
+      value: mockNotifier,
+      child: MaterialApp(
+        home: body,
+      ),
+    );
+  }
+
+  testWidgets(
+    'watchlist button should display add icon when tv not added to watchlist',
+    (WidgetTester tester) async {
+      // arrange
+      // act
+      // assert
+    },
+  );
+
+  testWidgets(
+    'watchlist button should display check icon when tv is added to watchlist',
+    (WidgetTester tester) async {
+      // arrange
+      // act
+      // assert
+    },
+  );
+
+  testWidgets(
+    'watchlist button should display snackbar when tv is added to watchlist',
+    (WidgetTester tester) async {
+      // arrange
+      // act
+      // assert
+    },
+  );
+
+  testWidgets(
+    'watchlist button should display alert dialog when add to watchlist failed',
+    (WidgetTester tester) async {
+      // arrange
+      // act
+      // assert
+    },
+  );
 }
