a729a29d94713d40f687b1d1f40ab94b856a0971
Your Name <you@example.com>
2022-03-14 16:37:11 +0800

    fix some bugs

--- movie/lib/presentation/pages/movie_detail_page.dart
+++ movie/lib/presentation/pages/movie_detail_page.dart
@@ -287,16 +287,6 @@ class MovieDetailContent extends StatelessWidget {
                       letterSpacing: 1.2,
                     ),
                   ),
-                  const SizedBox(height: 8.0),
-                  Text(
-                    movie.type_id.toString(),
-                    style: const TextStyle(
-                      color: Colors.white70,
-                      fontSize: 12.0,
-                      fontWeight: FontWeight.w500,
-                      letterSpacing: 1.2,
-                    ),
-                  ),
                 ],
               ),
             ),
@@ -326,30 +316,6 @@ class MovieDetailContent extends StatelessWidget {
         ),
       ],
     );
-  }
-
-  String _showGenres(List<Genre> genres) {
-    String result = '';
-    for (var genre in genres) {
-      result += genre.name + ', ';
-    }
-
-    if (result.isEmpty) {
-      return result;
-    }
-
-    return result.substring(0, result.length - 2);
-  }
-
-  String _showDuration(int runtime) {
-    final int hours = runtime ~/ 60;
-    final int minutes = runtime % 60;
-
-    if (hours > 0) {
-      return '${hours}h ${minutes}m';
-    } else {
-      return '${minutes}m';
-    }
   }
 
   Widget _showRecommendations() {
